47dd0444ff6af5c432303492f67fa421549d20fc
klester01 <klester01@aol.com>
2020-06-23 22:22:12 -0400

    adding bungee styling to CSS styling sheet and trivia rotating icon

--- src/components/Garden.js
+++ src/components/Garden.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Garden.css';
+import logo from './images/trivia.jpeg';
 
 function Garden()
 {
     return (
         <div className="Garden">
             <header className="Garden-header">
-                <p>
-                    Welcome to Science and Nature Trivia App.
-                </p>
+                <img src={logo} className="Garden-logo" alt="trivia" />
+                <div id="bungee">
+                    <p>Welcome to Science and Nature Trivia App!!</p>
+                    <p>Are you ready to test your knowledge?</p>
+                </div>
             </header>
         </div>
     );
