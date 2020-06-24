7b4e9f2cd8505eaa380f2dc913fc627a8347e37d
klester01 <klester01@aol.com>
2020-06-23 21:46:43 -0400

    adding Garden app and CSS files to repository. Styling can be updated

---
+++ src/components/Garden.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Garden.css';
+
+function Garden()
+{
+    return (
+        <div className="Garden">
+            <header className="Garden-header">
+                <p>
+                    Welcome to Science and Nature Trivia App.
+                </p>
+            </header>
+        </div>
+    );
+}
+export default Garden;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Garden from './components/Garden'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Garden />
   </React.StrictMode>,
   document.getElementById('root')
 );
