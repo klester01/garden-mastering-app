8cda4f16e9b82cebe6b8709fe028139dc085e01a
klester01 <klester01@aol.com>
2020-07-01 18:12:18 -0400

    got answers to render individually onto buttons

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,14 +6,17 @@ export class Question extends Component
     {
         return (
             <div>
-                <h2>
-                    {this.props.question.category}
-                </h2>
-                <div>
-                    <h2>
-                        {this.props.question.question}
-                    </h2>
-                </div>
+                <div><h2>{this.props.question.category}</h2></div>
+
+                <div><h2>Question: {this.props.question.question}</h2></div>
+
+                <div><p>A) <button><h2>{this.props.question.correct_answer}</h2></button></p></div>
+
+                <div><p>B) <button><h2>{this.props.question.incorrect_answers}</h2></button></p></div>
+
+                <div><p>C) <button><h2>{this.props.question.incorrect_answers[ 1 ]}</h2></button></p></div>
+
+                <div><p>D) <button><h2>{this.props.question.incorrect_answers[ 2 ]}</h2></button></p></div>
             </div>
 
         )
